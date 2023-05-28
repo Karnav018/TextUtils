81fa482982a5320898e7295816fa39976bbf1a01
Karnav018 <gandivdharikarnav@gmail.com>
2023-05-28 18:01:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+function App() {
+  return (
+    <>
+      <Navbar title="TextUtils" />
+      <TextForm heading="Enter the text for analyze below" />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+export default function TextForm(props) {
+  const [text, setText] = useState("Enter text here");
+  const HandleOnChange = (event) => {
+    console.log("Changed");
+    setText(event.target.value);
+  };
+  const HandleUpClick = () => {
+    let newText = text.toUpperCase();
+    console.log("Clicked" + text);
+    setText(newText);
+  };
+  const HandleLoClick = () => {
+    let newText = text.toLowerCase();
+    console.log("Clicked" + text);
+    setText(newText);
+  };
+  return (
+    <>
+      <div className="container">
+        <div className="mb-3">
+          <h1 className="fs-3 mt-3 text-center">{props.heading}</h1>
+          <textarea
+            className="form-control border-4 border-info rounded-1 "
+            id="exampleFormControlTextarea1"
+            rows="8"
+            value={text}
+            onChange={HandleOnChange}
+          ></textarea>
+        </div>
+        <div className="row">
+          <button className="btn btn-primary col mx-2" onClick={HandleUpClick}>
+            Uppercase
+          </button>
+          <button
+            className="btn btn-secondary col mx-2"
+            onClick={HandleLoClick}
+          >
+            Lowercase
+          </button>
+        </div>
+      </div>
+      <div className="container my-4 ">
+        <div className="bg-primary bg-opacity-10 text-center py-2 rounded-3 my-2">
+          <h2>Entered Text Summary</h2>
+          <p>
+            <b className="fs-4 text-danger">{text.split(" ").length}</b> words
+            and <b className="fs-4 text-danger">{text.length}</b> characters
+          </p>
+          <p>
+            <b className="fs-4 text-danger">{0.008 * text.split(" ").length}</b>{" "}
+            Minutes to read
+          </p>
+        </div>
+        <div className="bg-success bg-opacity-10 text-center py-2 rounded-3 my-2">
+          <h2 className="fw-bold fs-1">Preview</h2>
+          <p>{text}</p>
+        </div>
+      </div>
+    </>
+  );
+}
+TextForm.prototype = {
+  heading: PropTypes.string,
+  btnName: PropTypes.string,
+};
